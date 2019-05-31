9b2a6526e928e5cfbc10e73a713b4e0f05da62ce
PallaviVangari <vangaripallavi27@gmail.com>
2019-05-31 16:31:33 +0530

    Login Page with the validation has been added

--- Login/MainWindow.xaml.cs
+++ Login/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Login
 {
@@ -23,6 +25,34 @@ namespace Login
         public MainWindow()
         {
             InitializeComponent();
+            password.PasswordChar = '*';
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-HR2SEVF\SQLEXPRESS;Initial Catalog=my;User ID=sa;Password=pallavi");
+            string query = "select * from login_table where UserName='" + username.Text.Trim() + "'and Password1='" + password.Password.ToString().Trim() + "';";
+            SqlDataAdapter sd = new SqlDataAdapter(query, conn);
+            DataTable dt = new DataTable();
+            try
+            {
+                if (ConnectionState.Closed == conn.State)
+                    conn.Open();
+
+            }
+            catch
+            { }
+            sd.Fill(dt);
+            if(dt.Rows.Count == 1)
+            {
+                Form1 obj = new Login.Form1();
+                this.Hide();
+                obj.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password");
+            }
         }
     }
 }
